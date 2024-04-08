827b5a3afbdb6a3fedc675d01986e6bcbbbb3083
stefanglamocic <keksglamocic@gmail.com>
2024-04-09 01:18:06 +0200

    main app header

---
+++ main-app/src/app/header/dropdown-menu/dropdown-menu.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'dropdown-menu',
+  templateUrl: './dropdown-menu.component.html',
+  styleUrls: ['./dropdown-menu.component.css']
+})
+export class DropdownMenuComponent {
+
+}
